9f0ffae36060e93ed008535645e2b296502ce1e4
edangolgithub <dangolevan@gmail.com>
2018-10-28 09:16:31 +0545

    hgk

---
+++ src/evan/servlet/EvanGenericServlet.java
@@ -0,0 +1,23 @@
+package evan.servlet;
+
+import java.io.IOException;
+
+import javax.servlet.GenericServlet;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
+
+public class EvanGenericServlet extends GenericServlet {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+
+	@Override
+	public void service(ServletRequest req, ServletResponse res) throws ServletException, IOException {
+		res.getWriter().write("hello generic servlet");
+
+	}
+
+}
